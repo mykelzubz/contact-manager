8194d4900bf62813303c0623fc23560f52f45116
Michael <onyirims@yahoo.com>
2021-11-04 13:49:04 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import {PLATFORM} from 'aurelia-pal';
+
+
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Contacts';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title:'Select' },
+      { route: 'contacts/:id', moduleId: PLATFORM.moduleName('contact-detail'), title:'contacts' }
+    ]);
+
+    this.router = router;
+  }
+}
